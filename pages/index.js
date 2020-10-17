ac7c24d4fd956a8ba2aa424fb34121e4eb54123b
Guien Joris <49984305+guienjoris@users.noreply.github.com>
2020-10-17 10:46:58 +0200

    enter key input works

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import styled from 'styled-components';
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 
 const Input = styled.input`
@@ -30,21 +30,35 @@ const Title = styled.h1`
   text-shadow: 1px 1px 10px white;
   font-size: 100px;
 `
-
+const Signature = styled.a`
+  font-family: Orbitron, sans-serif;
+  color:white;
+  text-shadow: 1px 1px 10px white;
+  position:absolute;
+  bottom:10px;
+`
+const handleKeyUp = e=>{
+  console.log(e.currentTarget.value)
+  if (e.key === "Enter" && !e.shiftKey) {
+    console.log("vous avez appuyé sur enter")
+  }
+}
 export default function Home() {
   const searchInput = useRef(null);
 useEffect(()=>{
   searchInput.current.focus();
+  console.log(searchInput);
 },[]) 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Terminal site</title>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
       </Head>
       <Title>Terminal Site</Title>
-      <Input ref={searchInput}></Input>
+      <Input ref={searchInput} onKeyUp={handleKeyUp}></Input>
+      <Signature href="https://github.com/guienjoris" target="_blank" >Shynryu</Signature>
     </div>
   )
 }
